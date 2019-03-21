e05fdfa8a1e40c089c45cea859cc9c2009c1d091
alvincvms <>
2019-03-20 19:53:51 -0700

    half way thru ex_1
    getting close
    
    season 2 april 9th

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -1,29 +1,39 @@
 package _01_Intro_To_Sockets.client;
 
 import java.net.*;
+
+import _00_Click_Chat.networking.Server;
+
 import java.io.*;
 
 public class ClientGreeter {
-
+	
    public static void main(String [] args) {
+	   Server server;
+	   server = new Server(8080);
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+      String ip=server.getIPAddress();
       //2. Create an integer for the server's port number
-      
+      int port=8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
+    try {
+   	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
+    	 Socket sock = new Socket();
+        //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
+        DataOutputStream dos= new DataOutputStream(dos);
+        
+        //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
+        
+        //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
+        
+        //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
+        
+        //9. Close the client's server object
+	} catch (Exception e) {
+		
+	}
     
-    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
-         //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
-         //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
-         //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
-         //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
-         //9. Close the client's server object
 
    }
 }
